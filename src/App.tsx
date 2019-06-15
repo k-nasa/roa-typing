d2a46d953451b3c63d816ee06929d85d4b1677f6
k-nasa <htilcs1115@gmail.com>
2019-06-15 21:16:53 +0900

    feat: ちょっとしたページング処理を追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,39 @@ import './App.css';
 
 import logo from './logo.svg';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {date: new Date()};
+enum Pages {
+  Init,
+}
+
+interface IState {
+  page: Pages
+}
+
+class App extends React.Component<{}, IState> {
+  public state: IState = { page: Pages.Init};
+  constructor(props: {}){
+    super(props);
   }
 
   public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+    if (this.state.page === Pages.Init) {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
-      </div>
-    );
+        </div>
+      );
+    } 
+
+    // 該当ページなし
+    return (
+      <div>Sorry. Error</div>
+    )
   }
 }
 
