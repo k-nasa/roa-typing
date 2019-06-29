361d593cf8c5eabccf9c0dbbbea0260edb35b8ab
k-nasa <htilcs1115@gmail.com>
2019-06-29 14:36:59 +0900

    Add gameInner component

--- src/Game.tsx
+++ src/Game.tsx
@@ -18,5 +18,16 @@ const Game = () => {
         <p>ゲーム画面なのだ</p>
       </div>
     );
+const GameInner = ({ gameState }: IState) => {
+  if (gameState === GameState.Ready) {
+    return <p>Enterキーを押してゲームスタート</p>;
+  } else if (gameState === GameState.InAction) {
+    return <p>game main</p>;
+  } else if (gameState === GameState.Finish) {
+    return <p>result 画面表示</p>;
+  } else {
+    // tslint:disable-next-line:no-unused-variable
+    const check: never = gameState;
+    return <p>error {check}</p>;
   }
 }
